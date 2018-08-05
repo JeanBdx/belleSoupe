4191054fed7f911eaa828cb595d1b9453a2b448d
Jean <jean.arhancetebehere@gmail.com>
2018-08-05 18:52:27 +0200

    feat:removing node_modules

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.send('Hello Jean!')
 })
 
 app.listen(8080, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 8080!')
 })
